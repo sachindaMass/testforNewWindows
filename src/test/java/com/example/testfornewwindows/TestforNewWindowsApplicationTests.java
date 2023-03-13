cc826db0718e512aa631ab3d35a627cc656f18fa
Sachinda <sachinda46@gmail.com>
2023-03-13 14:15:11 -0700

    Test intellij 2022 with spring boot

---
+++ src/test/java/com/example/testfornewwindows/TestforNewWindowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfornewwindows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestforNewWindowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
